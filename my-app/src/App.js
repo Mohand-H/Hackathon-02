c0ef72f7f33b8ba6020571fca6048c6df22d64eb
Mohand H <mohandhassam1@gmail.com>
2018-05-17 09:58:22 +0200

    app

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Atelie from './Atelie';
+import './App.css';
+
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <Atelie />
+    );
+  }
+  }
+
+export default App;
