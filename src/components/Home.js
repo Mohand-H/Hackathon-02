6d352aee8f9ab8f1502f417115963e1b231af927
Helena Domlan <helena.domlan@gmail.com>
2018-05-17 21:28:23 +0200

    homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
 import React, { Component} from 'react';
 import './navbar.css'
+//import Homepic from './Homepic.js'
 import './Home.css'
 import {  Grid, Row, Col, Image} from 'react-bootstrap'
-import Navbar from "./navbar.js"
+//import Navbar from "./navbar.js"
+import Navbar from "./Match"
 import { Link} from 'react-router-dom'
 import Find from './Find.js'
 
@@ -14,13 +16,12 @@ render(){
         <div container-fluid>
          <Grid>
              <Navbar />
-            
             <Row>
             <Col xs={6} md={4}>
-           
             </Col>
             </Row>
         </Grid>
+        
         </div>
         
     )
